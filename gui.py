e49e2165b2258c589008fa02f19cce5161212f9a
stickman <ussread@ya.ru>
2022-11-24 02:59:02 +0300

    added gui checkbox selection

--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
 import asyncio
 import sys
 
+from PyQt5 import QtCore
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMainWindow, QAction, QApplication, QDesktopWidget, QFileDialog, qApp, \
-    QTextBrowser, QInputDialog, QLineEdit, QPushButton
+    QTextBrowser, QInputDialog, QLineEdit, QPushButton, QDialog, QMessageBox, QCheckBox, QLabel, QGridLayout, \
+    QVBoxLayout, QWidget
 
 import main
 
@@ -13,7 +15,6 @@ class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
-
 
     def initUI(self):
         self.statusBar()
@@ -37,12 +38,8 @@ class Example(QMainWindow):
         self.text_browser = QTextBrowser(self)
         self.text_browser.setReadOnly(True)
         self.text_browser.setOpenExternalLinks(True)
-        self.text_browser.setGeometry(10, 25, 700, 200)
+        self.text_browser.setGeometry(10, 25, 730, 200)
         self.text_browser.append('Hello!')
-
-        self.btn = QPushButton('Proceed', self)
-        self.btn.setGeometry(10, 250, 100, 20)
-        self.btn.clicked.connect(self.showInputDialog)
 
         self.resize(750, 450)
         self.center()
@@ -55,32 +52,73 @@ class Example(QMainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    def centerPos(self):
+        qr = QMainWindow.window(self).frameGeometry()
+        x = (qr.width() - self.width()) / 2
+        y = (qr.height() - self.height()) / 2
+        self.move(x, y)
+
     def showFileDialog(self):
         path_to_csv = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
         self.text_browser.append(path_to_csv)
         run(ex, path_to_csv)
 
-    def showInputDialog(self):
-        self.setGeometry(300, 300, 290, 150)
-        self.resize(750, 450)
-        self.center()
-        self.setWindowTitle('Input dialog')
-        self.show()
+    def createCheckBoxes(self, list_of_sites):
+        self.listCheckBox = [''] * len(list_of_sites)
+        self.listLabel = list_of_sites
+        self.title = QLabel()
+        self.title.setText('Select not working sites:')
+        grid = QGridLayout()
 
-        text, ok = QInputDialog.getText(self, 'Input Dialog',
-                                        'Input indexes:')
+        for i, v in enumerate(self.listLabel):
+            self.listCheckBox[i] = QCheckBox()
 
-        if ok:
-            print(str(text))
+            self.listLabel[i] = QLabel()
+            self.listLabel[i].setOpenExternalLinks(True)
+            self.listLabel[i].setText(f'<a href=\'{v}\'>{v}</a>')
+
+            grid.addWidget(self.title, 0, 0)
+            grid.addWidget(self.listCheckBox[i], i + 1, 0)
+            grid.addWidget(self.listLabel[i], i + 1, 1)
+
+        # self.button = QPushButton("Check CheckBox")
+        # self.button.clicked.connect(self.checkboxChanged)
+        # self.labelResult = QLabel()
+
+        # grid.addWidget(self.button, 10, 0, 1, 2)
+        # grid.addWidget(self.labelResult, 11, 0, 1, 2)
+        self.confirm.setLayout(grid)
+
+
+    def showInputDialog(self, list_of_urls):
+        # self.setGeometry(300, 300, 290, 150)
+        # self.resize(750, 450)
+        # self.center()
+        #
+        # text, ok = QInputDialog.getText(self, 'Input Dialog', 'Input indexes:')
+        #
+        # if ok:
+        #     print(str(text))
+        # else:
+        #     confirm = QMessageBox.question(self, 'Abort', 'Are you sure?', QMessageBox.Yes | QMessageBox.No)
+        #     if confirm == QMessageBox.No:
+        #         self.showInputDialog()
+
+        self.confirm = QDialog()
+        self.confirm.setWindowTitle('Check the following web-sites')
+        self.confirm.resize(300, 300)
+
+        self.createCheckBoxes(list_of_urls)
+
+        # self.confirm.setGeometry(300, 300, 290, 150)
+
+        self.confirm.show()
 
 
 def run(exm, path):
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main.multiprocessing_func(main.csv_list(), exm))
     bad_sites = {}
     csv_list = main.csv_list(path)
     length = len(csv_list)
-
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
@@ -88,15 +126,19 @@ def run(exm, path):
         loop.run_until_complete(main.multiprocessing_func(csv_list, exm, bad_sites, length))
     except KeyboardInterrupt:
         pass
-    list_to_check = main.get_list_of_check_sites(bad_sites)
+    list_to_check, bad_urls_list = main.get_list_of_check_sites(bad_sites)
 
-    if list_to_check is not None:
-        print('Check the following web-sites manually and input not working ones by their indexes:')
-        exm.text_browser.append('Check the following web-sites manually and input not working ones by their indexes:')
+    # if list_to_check is not None:
+    #     print('Check the following web-sites manually and input not working ones by their indexes:')
+    #     exm.text_browser.append('')
+    #     exm.text_browser.append('Check the following web-sites manually and input not working ones by their indexes:')
+    #
+    # for index_of_site, site_url in list_to_check.items():
+    #     print(f'{index_of_site}. {site_url}')
+    #     exm.text_browser.append(f'{index_of_site}. <a href=\'{site_url}\'>{site_url}</a>')
 
-    for index_of_site, site_url in list_to_check.items():
-        print(f'{index_of_site}. {site_url}')
-        exm.text_browser.append(f'{index_of_site}. <a href=\'{site_url}\'>{site_url}</a>')
+    exm.showInputDialog(list_to_check)
+    # print(list_to_check)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- main.py
+++ main.py
@@ -27,9 +27,7 @@ def append_dict(dictionary, key, value):
         dictionary[key] = values
 
 
-async def check(url, session, length, exm, bad_sites):
-
-
+async def check(url, session, bad_sites):
     session: aiohttp.ClientSession
     try:
         async with session.get(url, timeout=180) as response:
@@ -55,7 +53,7 @@ async def multiprocessing_func(url_list, exm, bad_sites, length):
     tasks = []
     async with aiohttp.ClientSession(trust_env=True) as session:
         for i in url_list:
-            tasks.append(asyncio.create_task(check(i, session, length, exm, bad_sites)))
+            tasks.append(asyncio.create_task(check(i, session, bad_sites)))
             # tasks.append(asyncio.create_task(check(i, session, len(url_list))))
             counter += 1
             exm.text_browser.append(f'{counter} of {length} sites answered')
@@ -66,73 +64,76 @@ async def multiprocessing_func(url_list, exm, bad_sites, length):
 
 def get_list_of_check_sites(bad_sites):
     bad_urls_list = list()
-    list_to_check = dict()
-    i = 1
+    list_to_check = list()
+    # list_to_check = dict()
+    # i = 1
     for err, urls in bad_sites.items():
         if err not in settings.critical_errs:
             for url in urls:
-                if url not in list_to_check.items():
-                    list_to_check[i] = url
+                if url not in list_to_check:
+                    # list_to_check[i] = url
+                    list_to_check.append(url)
         else:
             for url in urls:
                 if url not in bad_urls_list:
                     bad_urls_list.append(url)
-    return list_to_check
+    return list_to_check, bad_urls_list
 
 
-def remove_invalid_sites(exm, path_to_csv):
+# def remove_indexes():
+#     def check_int(s):
+#         if s[0] in ('-', '+'):
+#             return s[1:].isdigit()
+#         return s.isdigit()
+#
+#     for index_of_site, site_url in list_of_check_sites.items():
+#         print(f'{index_of_site}. {site_url}')
+#         exm.text_browser.append(f'{index_of_site}. {site_url}')
+#
+#
+#     print('Input indexes (empty str to exit): ')
+#     i = 0
+#     while i < len(list_of_check_sites.keys()):
+#         try:
+#             input_num = input()
+#             if not check_int(input_num):
+#                 print('Wrong index')
+#                 i = 0
+#             elif int(input_num) > len(list_of_check_sites):
+#                 print('Wrong index')
+#                 i = 0
+#             else:
+#                 print(input_num)
+#                 del list_of_check_sites[int(input_num)]
+#                 i += 1
+#         except (EOFError, IndexError):
+#             break
+#
+#     return list_of_check_sites
 
+# def remove_invalid_sites(path_to_csv):
 
-    list_of_check_sites = get_list_of_check_sites()
+    # list_of_check_sites = get_list_of_check_sites()
+    #
+    # if len(list_of_check_sites.values()) != 0:
+    #     print('Check the following web-sites manually and input not working ones by their indexes:')
+    #     exm.text_browser.append('Check the following web-sites manually and input not working ones by their indexes:')
+    #
+    # print()
+    #
+    # for index_of_site, site_url in list_of_check_sites.items():
+    #     print(f'{index_of_site}. {site_url}')
+    #     exm.text_browser.append(f'{index_of_site}. <a href=\'{site_url}\'>{site_url}</a>')
+    #     # exm.text_browser.append(f"<a href='{site_url}'>{site_url}</a>")
+    #
+    #
 
-    if len(list_of_check_sites.values()) != 0:
-        print('Check the following web-sites manually and input not working ones by their indexes:')
-        exm.text_browser.append('Check the following web-sites manually and input not working ones by their indexes:')
-
-    print()
-
-    for index_of_site, site_url in list_of_check_sites.items():
-        print(f'{index_of_site}. {site_url}')
-        exm.text_browser.append(f'{index_of_site}. <a href=\'{site_url}\'>{site_url}</a>')
-        # exm.text_browser.append(f"<a href='{site_url}'>{site_url}</a>")
-
-
-    def remove_indexes():
-        def check_int(s):
-            if s[0] in ('-', '+'):
-                return s[1:].isdigit()
-            return s.isdigit()
-
-        for index_of_site, site_url in list_of_check_sites.items():
-            print(f'{index_of_site}. {site_url}')
-            exm.text_browser.append(f'{index_of_site}. {site_url}')
-
-
-        print('Input indexes (empty str to exit): ')
-        i = 0
-        while i < len(list_of_check_sites.keys()):
-            try:
-                input_num = input()
-                if not check_int(input_num):
-                    print('Wrong index')
-                    i = 0
-                elif int(input_num) > len(list_of_check_sites):
-                    print('Wrong index')
-                    i = 0
-                else:
-                    print(input_num)
-                    del list_of_check_sites[int(input_num)]
-                    i += 1
-            except (EOFError, IndexError):
-                break
-
-        return list_of_check_sites
 
     # remove_indexes()
 
     # for url in list_of_check_sites.values():
     #     bad_urls_list.append(url)
-    #
+
     # with open(path_to_csv, 'r') as input_csv_file:
     #     csv_into_list = []
     #     reader = csv.reader(input_csv_file)
@@ -145,7 +146,6 @@ def remove_invalid_sites(exm, path_to_csv):
     #     for rows in csv_into_list:
     #         if rows[1] not in bad_urls_list:
     #             writer.writerow(rows)
-    exm.text_browser.append('Done')
 
 
 
