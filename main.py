3aac5ba8105028579493a7a4152590ee0a3f3a37
RLAB\nzhukov <nzhukov@romance.iki.rssi.ru>
2022-11-28 19:07:35 +0300

    added multithreading support

--- main.py
+++ main.py
@@ -1,110 +1,173 @@
 import asyncio
-import csv
 import os
 import sys
+import traceback
 
-import aiohttp
-import settings
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import QThread
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtWidgets import QMainWindow, QAction, QApplication, QDesktopWidget, QFileDialog, qApp, \
+    QTextBrowser, QInputDialog, QLineEdit, QPushButton, QDialog, QMessageBox, QCheckBox, QLabel, QGridLayout, \
+    QVBoxLayout, QWidget
 
-# path_to_csv = 'C:\\Users\\ussre\\Documents\\ChromePasswords.csv'
-# path_to_new_csv = "D:\\Documents\\new_ChromePasswords.csv"
+import processing
 
+class Example(QMainWindow):
 
-def csv_list(path_to_csv):
-    with open(path_to_csv, 'r', encoding='utf-8') as csvfile:
-        pwd_list = []
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            pwd_list.append(row['url'])
-        return pwd_list
+    def initUI(self):
+        self.statusBar()
+        menubar = self.menuBar()
 
+        openFile = QAction(QIcon("icons/62917-open-file-folder-icon.png"), 'Open', self)
+        openFile.setShortcut('Ctrl+O')
+        openFile.setStatusTip('Open new File')
+        openFile.triggered.connect(self.showFileDialog)
 
-def append_dict(dictionary, key, value):
-    if key in dictionary.keys():
-        dictionary[key].append(value)
-    else:
-        values = [value]
-        dictionary[key] = values
+        exitAction = QAction(QIcon("icons/png-red-round-close-x-icon-31631915146jpppmdzihs.png")
+                             , '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
 
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(openFile)
+        fileMenu.addAction(exitAction)
 
-async def check(url, session, bad_sites):
-    session: aiohttp.ClientSession
-    try:
-        async with session.get(url, timeout=180) as response:
-            status_code = str(response.status)
+        font = QFont('Times', 12)
 
-            # print(url + ' ' + status_code)
-            if status_code.startswith('5') | int(status_code) == 404:
-                append_dict(bad_sites, status_code, url)
+        self.text_browser = QTextBrowser(self)
+        self.text_browser.setReadOnly(True)
+        self.text_browser.setOpenExternalLinks(True)
+        self.text_browser.setGeometry(10, 25, 730, 200)
+        self.text_browser.setFont(font)
 
-            return status_code
-    except:
-        exc_msg = str(sys.exc_info()[0])
-        # print(url + ' ' + exc_msg)
-        append_dict(bad_sites, exc_msg, url)
+        self.text_browser.append('Hello!')
 
-    return bad_sites
-
-
-async def multiprocessing_func(url_list, exm, bad_sites, length):
-    global counter
-    counter = 0
-
-    tasks = []
-    async with aiohttp.ClientSession(trust_env=True) as session:
-        for i in url_list:
-            tasks.append(asyncio.create_task(check(i, session, bad_sites)))
-            # tasks.append(asyncio.create_task(check(i, session, len(url_list))))
-            counter += 1
-            exm.text_browser.append(f'{counter} of {length} sites answered')
-            print(f'{counter} of {length} sites answered')
-
-        return await asyncio.gather(*tasks)
+        self.resize(750, 450)
+        self.center()
+        self.setWindowTitle('Parse alive sites')
+        self.show()
 
 
-def get_list_of_check_sites(bad_sites):
-    bad_urls_list = list()
-    list_to_check = list()
-    # list_to_check = dict()
-    # i = 1
-    for err, urls in bad_sites.items():
-        if err not in settings.critical_errs:
-            for url in urls:
-                if url not in list_to_check:
-                    # list_to_check[i] = url
-                    list_to_check.append(url)
-        else:
-            for url in urls:
-                if url not in bad_urls_list:
-                    bad_urls_list.append(url)
-    return list_to_check, bad_urls_list
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def centerPos(self):
+        qr = QMainWindow.window(self).frameGeometry()
+        x = (qr.width() - self.width()) / 2
+        y = (qr.height() - self.height()) / 2
+        self.move(x, y)
+
+    def showFileDialog(self):
+        global path_to_csv
+        path_to_csv = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
+        self.text_browser.append(path_to_csv)
+        self.doAction(ex)
+        # run(ex)
 
 
-def remove_invalid_sites(bad_urls_list, path_to_csv):
-    def newFilePath(file_path):
-        head, tail = os.path.split(file_path)
-        new_file_path = head + '/new_' + tail
-        return new_file_path
+    def createCheckBoxes(self, list_of_sites, bad_urls_list):
+        self.resultList = bad_urls_list
 
-    with open(path_to_csv, 'r', encoding='utf-8') as input_csv_file:
-        csv_into_list = []
-        reader = csv.reader(input_csv_file)
-        for row in reader:
-            csv_into_list.append(row)
+        self.listCheckBox = [''] * len(list_of_sites)
+        self.listLabel = list_of_sites
 
-    with open(newFilePath(path_to_csv), 'w', newline='',
-              encoding='utf-8') as output_csv_file:
-        writer = csv.writer(output_csv_file)
-        for rows in csv_into_list:
-            if rows[1] not in bad_urls_list:
-                writer.writerow(rows)
+        self.title = QLabel()
+        self.title.setText('Select not working sites:')
+
+        grid = QGridLayout()
+        grid.addWidget(self.title, 0, 0, 1, 40)
+
+        for i, v in enumerate(self.listLabel):
+            self.listCheckBox[i] = QCheckBox()
+
+            self.listLabel[i] = QLabel()
+            self.listLabel[i].setOpenExternalLinks(True)
+            self.listLabel[i].setText(f'<a href=\'{v}\'>{v}</a>')
 
 
-# start_time = time.time()
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(multiprocessing_func(csv_list()))
-# remove_invalid_sites()
+            grid.addWidget(self.listCheckBox[i], i + 1, 1)
+            grid.addWidget(self.listLabel[i], i + 1, 2)
 
-# print(f'--- {round((time.time() - start_time) / 60, 2)} minutes ---')
+        self.buttonOk = QPushButton("OK")
+        self.buttonOk.clicked.connect(self.checkboxChanged)
+
+        grid.addWidget(self.buttonOk, len(list_of_sites) + 10, 0, 1, 2)
+
+        self.confirm.setLayout(grid)
+
+    def checkboxChanged(self):
+        for i, v in enumerate(self.listCheckBox):
+            doc = QtGui.QTextDocument()
+            doc.setHtml(self.listLabel[i].text())
+            text = doc.toRawText()
+            if v.checkState():
+                self.resultList.append(text)
+        self.confirm.close()
+        processing.remove_invalid_sites(self.resultList, path_to_csv)
+        self.text_browser.append('Done!')
+
+
+    def doAction(self, window):
+        bad_sites = {}
+        csv_list = processing.csv_list(path_to_csv)
+        length = len(csv_list)
+        self.worker = MainBackgroundThread(csv_list, window, bad_sites, length)
+        self.worker.finished.connect(self.inputDialog)
+        self.worker.start()
+
+
+    def inputDialog(self):
+        self.confirm = QDialog()
+        self.confirm.setWindowTitle('Check the following web-sites')
+        self.confirm.resize(200, 100)
+        list_to_check, bad_urls_list = processing.get_list_of_check_sites(self.worker.bad_sites)
+        self.createCheckBoxes(list_to_check, bad_urls_list)
+        self.confirm.show()
+
+
+class MainBackgroundThread(QThread):
+    def __init__(self, csv_list, window, bad_sites, length):
+        QThread.__init__(self)
+        self.csv_list, self.window, self.bad_sites, self.length = csv_list, window, bad_sites, length
+
+    def run(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(processing.multiprocessing_func(self.csv_list, self.window,
+                                                                self.bad_sites, self.length))
+        loop.run_until_complete(asyncio.sleep(0.250))
+        loop.close()
+
+        def task_result(tasks):
+            res = []
+            for t in tasks:
+                try:
+                    r = t.result()  # gives an error!!!
+                except BaseException as e:
+                    res.append(e)
+                    continue
+                res.append(r)
+
+            print(res)
+
+# if QtCore.QT_VERSION >= 0x50501:
+#     def excepthook(type_, value, traceback_):
+#         traceback.print_exception(type_, value, traceback_)
+#         QtCore.qFatal('')
+# sys.excepthook = excepthook
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
+
 
---
+++ processing.py
@@ -0,0 +1,118 @@
+import asyncio
+import csv
+import os
+import sys
+
+import aiohttp
+import settings
+
+# path_to_csv = 'C:\\Users\\ussre\\Documents\\ChromePasswords.csv'
+# path_to_new_csv = "D:\\Documents\\new_ChromePasswords.csv"
+
+
+def csv_list(path_to_csv):
+    with open(path_to_csv, 'r', encoding='utf-8') as csvfile:
+        pwd_list = []
+
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            pwd_list.append(row['url'])
+        return pwd_list
+
+
+def append_dict(dictionary, key, value):
+    if key in dictionary.keys():
+        dictionary[key].append(value)
+    else:
+        values = [value]
+        dictionary[key] = values
+
+
+async def check(url, session, bad_sites, exm, length):
+    global counter
+    counter = 0
+
+    session: aiohttp.ClientSession
+    try:
+        async with session.get(url, timeout=180) as response:
+            status_code = str(response.status)
+
+            if status_code.startswith('5') | int(status_code) == 404:
+                append_dict(bad_sites, status_code, url)
+            # return status_code
+    except:
+        exc_msg = str(sys.exc_info()[0])
+        append_dict(bad_sites, exc_msg, url)
+
+    counter += 1
+    exm.text_browser.append(f'{counter} of {length} sites answered')
+    print(f'{counter} of {length} sites answered')
+    return bad_sites
+
+
+async def multiprocessing_func(url_list, exm, bad_sites, length):
+    # global counter
+    # counter = 0
+
+    tasks = []
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        for i in url_list:
+            future = asyncio.ensure_future(check(i, session, bad_sites, exm, length))
+            tasks.append(future)
+            while future.done():
+                print(future.result())
+
+            # tasks.append(asyncio.create_task(check(i, session, bad_sites, exm, length)))
+
+            # counter += 1
+            # exm.text_browser.append(f'{counter} of {length} sites answered')
+            # print(f'{counter} of {length} sites answered')
+
+        return await asyncio.gather(*tasks)
+
+
+def get_list_of_check_sites(bad_sites):
+    bad_urls_list = list()
+    list_to_check = list()
+    # list_to_check = dict()
+    # i = 1
+    for err, urls in bad_sites.items():
+        if err not in settings.critical_errs:
+            for url in urls:
+                if url not in list_to_check:
+                    # list_to_check[i] = url
+                    list_to_check.append(url)
+        else:
+            for url in urls:
+                if url not in bad_urls_list:
+                    bad_urls_list.append(url)
+    return list_to_check, bad_urls_list
+
+
+def remove_invalid_sites(bad_urls_list, path_to_csv):
+    def newFilePath(file_path):
+        head, tail = os.path.split(file_path)
+        new_file_path = head + '/new_' + tail
+        return new_file_path
+
+    with open(path_to_csv, 'r', encoding='utf-8') as input_csv_file:
+        csv_into_list = []
+        reader = csv.reader(input_csv_file)
+        for row in reader:
+            csv_into_list.append(row)
+
+    with open(newFilePath(path_to_csv), 'w', newline='',
+              encoding='utf-8') as output_csv_file:
+        writer = csv.writer(output_csv_file)
+        for rows in csv_into_list:
+            if rows[1] not in bad_urls_list:
+                writer.writerow(rows)
+
+
+# start_time = time.time()
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(multiprocessing_func(csv_list()))
+# remove_invalid_sites()
+
+# print(f'--- {round((time.time() - start_time) / 60, 2)} minutes ---')
+
